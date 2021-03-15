5c0ad42277d889370e313bcb33dc2a3ba4565af2
saku22 <saku.kekki@edu.hollola.fi>
2021-03-15 11:45:08 +0000

    2021315

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex', maxWidth: 1900 }}>
+        <Chart
+          width={1400}
+          height={300}
+          chartType="ColumnChart"
+          loader={<div>Loading Chart</div>}
+          data={[
+            ['City', '2010 Population'],
+            ['New York City, NY', 8175000],
+            ['Los Angeles, CA', 3792000],
+            ['Chicago, IL', 2695000],
+            ['Houston, TX', 2099000],
+            ['Philadelphia, PA', 1526000],
+          ]}
+          options={{
+            title: 'Population of Largest U.S. Cities',
+            chartArea: { width: '30%' },
+            hAxis: {
+              title: 'Total Population',
+              minValue: 0,
+            },
+            vAxis: {
+              title: 'City',
+            },
+          }}
+          legendToggle
+        />
+        </div>
+        <div style={{ display: 'flex', maxWidth: 900 }}>
+        <Chart
+          width={400}
+          height={'300px'}
+          chartType="AreaChart"
+          loader={<div>Loading Chart</div>}
+          data={[
+            ['Year', 'Sales', 'Expenses'],
+            ['2013', 1000, 400],
+            ['2014', 1170, 460],
+            ['2015', 660, 1120],
+            ['2016', 1030, 540],
+          ]}
+          options={{
+            title: 'Company Performance',
+            hAxis: { title: 'Year', titleTextStyle: { color: '#333' } },
+            vAxis: { minValue: 0 },
+            // For the legend to fit, we make the chart area smaller
+            chartArea: { width: '50%', height: '70%' },
+            // lineWidth: 25
+          }}
+        />
+      </div>
+
     </div>
   );
 }
